1f8e5eee58105246bc936fa030ccdfa51ecf84c0
baqirsaq <emiri.baqirsaq@gmail.com>
2026-02-23 15:31:30 +0100

    update gravity character_class.gd

--- Classes/character_class.gd
+++ Classes/character_class.gd
@@ -5,9 +5,10 @@ extends CharacterBody2D
 var speed: float = 0.0
 var acceleration: float = 0.0
 var deceleration: float = 0.0
-var current_health: int = 1000
+var current_health: int = 100
+var _current_gravity: float
 var _gravity_min: float = 920.0
-var _gravity_max: float = 1100.0
+var _gravity_max: float = 1500.0
 
 
 func _ready() -> void:
@@ -17,7 +18,11 @@ func _ready() -> void:
 
 func apply_gravity(delta) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += _current_gravity * delta
+		if velocity.y > 0:
+			_current_gravity = move_toward(_current_gravity, _gravity_max, 300)
+		else:
+			_current_gravity = _gravity_min
 
 func lose_health(value: int) -> void:
 	current_health -= value
